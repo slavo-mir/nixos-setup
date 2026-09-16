53104a926754d8aa7b6942917097b40d20b8ee0b
slavo-mir <222432419+slavo-mir@users.noreply.github.com>
2026-09-16 13:49:29 +0200

    session update

--- modules/desktop/session.nix
+++ modules/desktop/session.nix
@@ -1,6 +1,10 @@
 { pkgs, ... }: {
   services.getty.autologinUser = "slavomir";
 
+  environment.sessionVariables = {
+    _JAVA_AWT_WM_NONREPARENTING = "1";
+  };
+
   programs.fish.loginShellInit = ''
     if test -z "$DISPLAY"; and test (tty) = /dev/tty1
       exec dbus-run-session sway --unsupported-gpu
