c7d3a30a4908eb9817139f0be337f8cc291f3995
slavo-mir <222432419+slavo-mir@users.noreply.github.com>
2026-09-16 18:30:22 +0200

    session update

--- modules/desktop/session.nix
+++ modules/desktop/session.nix
@@ -3,6 +3,19 @@
 
   environment.sessionVariables = {
     _JAVA_AWT_WM_NONREPARENTING = "1";
+
+    XDG_SESSION_TYPE = "wayland";
+    XDG_CURRENT_DESKTOP = "sway";
+    XDG_SESSION_DESKTOP = "sway";
+
+    MOZ_ENABLE_WAYLAND = "1";
+    QT_QPA_PLATFORM = "wayland";
+    GDK_BACKEND = "wayland,x11";
+    SDL_VIDEODRIVER = "wayland";
+    CLUTTER_BACKEND = "wayland";
+    NIXOS_OZONE_WL = "1";
+
+    WLR_NO_HARDWARE_CURSOR = "1";
   };
 
   programs.fish.loginShellInit = ''
