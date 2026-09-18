23d2136a81a297a415a5eba81ed30e193bbdfb62
slavo-mir <222432419+slavo-mir@users.noreply.github.com>
2026-09-18 09:39:16 +0200

    permissions

--- modules/desktop/dotfiles.nix
+++ modules/desktop/dotfiles.nix
@@ -1,16 +1,16 @@
 { ... }: {
   systemd.user.tmpfiles.rules = [
-    "C %h/.config/alacritty       - - - - /etc/nixos/dotfiles/alacritty"
-    "C %h/.config/gtk-3.0         - - - - /etc/nixos/dotfiles/gtk-3.0"
-    "C %h/.config/mako            - - - - /etc/nixos/dotfiles/mako"
-    "C %h/.config/nvim            - - - - /etc/nixos/dotfiles/nvim"
-    "C %h/.config/sway            - - - - /etc/nixos/dotfiles/sway"
-    "C %h/.config/tmux            - - - - /etc/nixos/dotfiles/tmux"
-    "C %h/.config/fish/functions  - - - - /etc/nixos/dotfiles/fish/functions"
-    "C %h/.config/git             - - - - /etc/nixos/dotfiles/git"
+    "C %h/.config/alacritty       0700 - - - /etc/nixos/dotfiles/alacritty"
+    "C %h/.config/gtk-3.0         0700 - - - /etc/nixos/dotfiles/gtk-3.0"
+    "C %h/.config/mako            0700 - - - /etc/nixos/dotfiles/mako"
+    "C %h/.config/nvim            0700 - - - /etc/nixos/dotfiles/nvim"
+    "C %h/.config/sway            0700 - - - /etc/nixos/dotfiles/sway"
+    "C %h/.config/tmux            0700 - - - /etc/nixos/dotfiles/tmux"
+    "C %h/.config/fish/functions  0700 - - - /etc/nixos/dotfiles/fish/functions"
+    "C %h/.config/git             0700 - - - /etc/nixos/dotfiles/git"
 
     "d %h/.ssh          0700 - - -"
     "C %h/.ssh/config   0600 - - - /etc/nixos/dotfiles/ssh/config"
-    "C %h/.gitconfig    0644 - - - /etc/nixos/dotfiles/git/gitconfig"
+    "C %h/.gitconfig    0600 - - - /etc/nixos/dotfiles/git/gitconfig"
   ];
 }
