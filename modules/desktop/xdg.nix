45a01a32fe2ab93d982b21a8b526bc86064192a5
slavo-mir <222432419+slavo-mir@users.noreply.github.com>
2026-09-18 09:37:29 +0200

    permissions

--- modules/desktop/xdg.nix
+++ modules/desktop/xdg.nix
@@ -1,34 +1,34 @@
 { config, pkgs, ... }: {
   xdg.portal = {
     enable = true;
-    extraPortals = [ 
+    extraPortals = [
       pkgs.xdg-desktop-portal-wlr
     ];
-    
+
     config = {
       common = {
         default = [
-	        "wlr"
-	      ];
+          "wlr"
+        ];
       };
     };
   };
 
   systemd.user.tmpfiles.rules = [
-    "d %h/Documents 0755 - - -"
-    "d %h/Downloads 0755 - - -"
-    "d %h/Music 0755 - - -"
-    "d %h/Pictures 0755 - - -"
-    "d %h/Videos 0755 - - -"
+    "d %h/Documents 0700 - - -"
+    "d %h/Downloads 0700 - - -"
+    "d %h/Music 0700 - - -"
+    "d %h/Pictures 0700 - - -"
+    "d %h/Videos 0700 - - -"
 
-    "d %h/Music/fetched 0755 - - -"
-    "d %h/Pictures/screenshots 0755 - - -"
-    "d %h/Videos/fetched 0755 - - -"
-    "d %h/Videos/recordings 0755 - - -"
+    "d %h/Music/fetched 0700 - - -"
+    "d %h/Pictures/screenshots 0700 - - -"
+    "d %h/Videos/fetched 0700 - - -"
+    "d %h/Videos/recordings 0700 - - -"
 
-    "d %h/Projects 0755 - - -"
-    "d %h/Projects/personal 0755 - - -"
-    "d %h/Projects/local 0755 - - -"
-    "d %h/Projects/uni 0755 - - -"
+    "d %h/Projects 0700 - - -"
+    "d %h/Projects/personal 0700 - - -"
+    "d %h/Projects/local 0700 - - -"
+    "d %h/Projects/uni 0700 - - -"
   ];
 }
